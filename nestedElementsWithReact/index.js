daa1e68ce17e4a38d9ad88d8efd71dffd2bf1365
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-21 11:59:38 +0200

    3434

---
+++ nestedElementsWithReact/index.js
@@ -0,0 +1,24 @@
+const root = document.querySelector("#root");
+
+const element = React.createElement(
+  "div",
+  {
+    className: "greeting",
+  },
+  React.createElement(
+    "div",
+    {
+      className: "greeting__title",
+    },
+    "Hello, World!"
+  ),
+  React.createElement(
+    "div",
+    {
+      className: "greeting__text",
+    },
+    "I'm learning React"
+  )
+);
+
+ReactDOM.render(element, root);
