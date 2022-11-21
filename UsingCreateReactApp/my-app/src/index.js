5a505bb70f31040a95db685ba99f84d2bbf98677
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-21 14:20:42 +0200

    2323

---
+++ UsingCreateReactApp/my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const element = (
+  <div className="greeting">
+    <div className="greeting__title">Hello, world!</div>
+    <div className="greeting__text">I'm learning React</div>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
