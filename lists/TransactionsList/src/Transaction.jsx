ec67048e5285156f39d6a0d1d1e85aece7622988
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-28 21:04:08 +0200

    3434

--- lists/TransactionsList/src/Transaction.jsx
+++ lists/TransactionsList/src/Transaction.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import moment from "moment";
 
+const numFormat = (num) => {
+  return new Intl.NumberFormat("en-GB").format(num);
+};
+
 class Transaction extends Component {
   constructor(props) {
     super(props);
@@ -19,12 +23,8 @@ class Transaction extends Component {
         <span className="transaction__assets">
           {from} {to}
         </span>
-        <span className="transaction__rate">
-          {new Intl.NumberFormat("en-GB").format(rate)}
-        </span>
-        <span className="transaction__amount">
-          {new Intl.NumberFormat("en-GB").format(amount)}
-        </span>
+        <span className="transaction__rate">{numFormat(rate)}</span>
+        <span className="transaction__amount">{numFormat(amount)}</span>
       </li>
     );
   }
