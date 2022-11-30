dfdcf1e440db781348f7a6b787a75386509e0740
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-30 12:45:24 +0200

    2323

---
+++ LifecycleMethods/methods/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Life from "./Life";
+
+const rootElement = document.querySelector("#root");
+
+ReactDOM.render(<Life />, rootElement);
