ed16dfed1ac0f472366eebd07372886935c76587
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-12-01 11:56:18 +0200

    232323

---
+++ forms/ControlledComponent/src/Search.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      value: "",
+    };
+    this.onChangeInput = this.onChangeInput.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChangeInput(e) {
+    this.setState({
+      value: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    alert(`Search text: ${this.state.value}`);
+  }
+  render() {
+    return (
+      <form className="search">
+        <input
+          type="text"
+          className="search__input"
+          onChange={this.onChangeInput}
+          value={this.state.value}
+        />
+        <button
+          className="search__button"
+          type="submit"
+          onClick={this.onSubmit}
+        >
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Search;
---
+++ forms/ControlledComponent/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.scss";
+
+import Search from "./Search";
+
+const root = document.querySelector("#root");
+
+ReactDom.render(<Search />, root);
