66f11530b824cded8dfa4dbf523a834814d0b852
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-12-05 16:27:07 +0200

    121212

---
+++ componentUpdate/propsChildren/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+import Dialog from "./Dialog";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+
+    this.toggleDialog = this.toggleDialog.bind(this);
+  }
+
+  toggleDialog() {
+    this.setState({
+      isOpen: (this.state.isOpen = !this.state.isOpen),
+    });
+  }
+
+  render() {
+    const { isOpen } = this.state;
+    return (
+      <div class="app">
+        <button class="btn" onClick={this.toggleDialog}>
+          Show dialog
+        </button>
+        <Dialog
+          title={"Recommendation"}
+          isOpen={isOpen}
+          onClose={this.toggleDialog}
+        >
+          <p>
+            Use immutable array methods to work with data. It will help to avoid
+            bugs
+          </p>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default App;
