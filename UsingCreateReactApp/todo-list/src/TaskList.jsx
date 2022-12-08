69fdc467cb9d09d0941a1165ff83eb07a139e597
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-12-08 13:20:05 +0200

    12123

--- UsingCreateReactApp/todo-list/src/TaskList.jsx
+++ UsingCreateReactApp/todo-list/src/TaskList.jsx
@@ -16,62 +16,56 @@ class TaskList extends Component {
     this.addNewTask = this.addNewTask.bind(this);
     this.changeTaskStatus = this.changeTaskStatus.bind(this);
     this.deleteTask = this.deleteTask.bind(this);
+    this.loadDataFromServer = this.loadDataFromServer.bind(this);
   }
 
-  addNewTask(value) {
-    const newTask = {
-      task: value,
-      status: false,
-    };
-    createNewTask(newTask)
-      .then(() => {
-        getAllTasks().then((tasks) =>
-          this.setState({
-            tasks,
-          })
-        );
-      })
-      .catch((e) => {
-        throw new Error(e);
+  async addNewTask(value) {
+    try {
+      const newTask = {
+        task: value,
+        status: false,
+      };
+      await createNewTask(newTask);
+      this.loadDataFromServer();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async changeTaskStatus(id) {
+    try {
+      const { tasks } = this.state;
+      let { task, status } = tasks.find((task) => task.id === id);
+      await updateStatusOfExistingTask(id, {
+        task,
+        status: !status,
       });
+      this.loadDataFromServer();
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
-  changeTaskStatus(id) {
-    const { tasks } = this.state;
-    let { task, status } = tasks.find((task) => task.id === id);
-    updateStatusOfExistingTask(id, { task, status: !status }).then(() => {
-      getAllTasks().then((tasks) =>
-        this.setState({
-          tasks,
-        })
-      );
-    });
+  async deleteTask(id) {
+    try {
+      await deleteDeprecatedTask(id);
+      this.loadDataFromServer();
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
-  deleteTask(id) {
-    deleteDeprecatedTask(id)
-      .then(() => {
-        getAllTasks().then((tasks) =>
-          this.setState({
-            tasks,
-          })
-        );
-      })
-      .catch((e) => {
-        throw new Error(e);
-      });
+  async loadDataFromServer() {
+    try {
+      const tasks = await getAllTasks();
+      this.setState({ tasks });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
   componentDidMount() {
-    getAllTasks()
-      .then((data) =>
-        this.setState({
-          tasks: data,
-        })
-      )
-      .catch((e) => {
-        throw new Error(e);
-      });
+    this.loadDataFromServer();
   }
 
   render() {
