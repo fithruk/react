4fcdaa5f017b6458ad0810b42bed0f5883d22f6f
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-12-09 16:12:06 +0200

    22323

---
+++ hooks/counter/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+import Counter from "./Counter";
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
